a57e007d3abffd939f573cb834e4cc6085c47482
eduardosolid <mx-velocidad@hotmail.com>
2016-04-04 00:50:42 -0500

    se agrega un ejemplo de la lectura de la imagen por pixeles con openCV

--- src/ui/adminwindow.cpp
+++ src/ui/adminwindow.cpp
@@ -82,8 +82,78 @@ void AdminWindow::on_addButton_clicked(){
 
 void AdminWindow::on_pushButton_clicked()
 {
-    qDebug( "hola numeros: %d", 12345 );
-    cv::Mat inputImage = cv::imread("/home/luis/Documentos/SatellitalImages/LE70270472003048EDC00/LE70270472003048EDC00_B1.TIF");
+    std::string rutaTIFF_1 = "/home/luis/Documentos/SatellitalImages/LE70270472003048EDC00/LE70270472003048EDC00_B1.TIF";
+    std::string rutaTIFF_2 = "/home/luis/Documentos/SatellitalImages/LE70270472003048EDC00/LE70270472003048EDC00_B2.TIF";
+    std::string rutaTIFF_3 = "/home/luis/Documentos/SatellitalImages/LE70270472003048EDC00/LE70270472003048EDC00_B3.TIF";
+    std::string rutaLena = "/home/luis/Descargas/sample-opencv/lena.jpg";
+    /* http://docs.opencv.org/3.0-beta/modules/imgcodecs/doc/reading_and_writing_images.html
+    flags –
+    Flags specifying the color type of a loaded image:
 
-    qDebug( "el total() es: %d", inputImage.total() );
+        CV_LOAD_IMAGE_ANYDEPTH - If set, return 16-bit/32-bit image when the input has the corresponding depth, otherwise convert it to 8-bit.
+        CV_LOAD_IMAGE_COLOR - If set, always convert image to the color one
+        CV_LOAD_IMAGE_GRAYSCALE - If set, always convert image to the grayscale one
+
+        >0 Return a 3-channel color image.
+            Note
+            In the current implementation the alpha channel, if any, is stripped from the output image. Use negative value if you need the alpha channel.
+        =0 Return a grayscale image.
+        <0 Return the loaded image as is (with alpha channel).
+   */
+    cv::Mat inputImage_1 = cv::imread(rutaTIFF_1, CV_LOAD_IMAGE_GRAYSCALE);
+    cv::Mat inputImage_2 = cv::imread(rutaTIFF_2, CV_LOAD_IMAGE_GRAYSCALE);
+    cv::Mat inputImage_3 = cv::imread(rutaTIFF_3, CV_LOAD_IMAGE_GRAYSCALE);
+    cv::Mat inputLena = cv::imread(rutaLena);
+
+    std::vector<cv::Mat> array_to_merge;
+    array_to_merge.push_back(inputImage_1); //B
+    array_to_merge.push_back(inputImage_2); //G
+    array_to_merge.push_back(inputImage_3); //R
+
+    cv::Mat final_color; // aqui queda la imagen a color de las 3 bandas :)
+
+    cv::merge(array_to_merge, final_color);
+    cv::imshow("a colorcin",final_color);
+
+    qDebug( "total() es: %d", inputImage_1.total() );
+    qDebug( "depth() es: %d", inputImage_1.depth() );
+    qDebug( "channels() es: %d", inputImage_1.channels() );
+    qDebug( "size().width es: %d", inputImage_1.size().width );
+    qDebug( "size().height es: %d", inputImage_1.size().height );
+    qDebug( "size().area es: %d", inputImage_1.size().area() );
+    //qDebug( "begin() es: %d", inputImage.begin() );
+    //qDebug( "end() es: %d", inputImage.end() );
+
+    /* Del siguiente link esta la referencia del codigo de lectura :)
+     * http://longstryder.com/2014/07/which-way-of-accessing-pixels-in-opencv-is-the-fastest/
+     * en el siguiente tambien hay algo de info :) que no es bueno usar .at()
+     * http://stackoverflow.com/questions/11977954/opencv-matrix-iteration
+     * http://stackoverflow.com/questions/25221421/c-opencv-fast-pixel-iteration
+     * http://stackoverflow.com/questions/28423701/efficient-way-to-loop-through-pixels-of-16-bit-mat-in-opencv
+     * http://answers.opencv.org/question/38/what-is-the-most-effective-way-to-access-cvmat-elements-in-a-loop/
+     * +++++ y como cabe destacar usé la lectura eficient, la segura esta en el siguiente link
+     * http://docs.opencv.org/2.4/doc/tutorials/core/how_to_scan_images/how_to_scan_images.html
+     * pero es mas lenta xD :D
+    */
+qDebug( "rows: %d, cols: %d", inputLena.rows, inputLena.cols );
+    //vamos a iterar la imagen de 3 bandas
+    for (int row = 0; row < inputLena.rows; ++row)
+    {
+        const uchar *ptr = inputLena.ptr(row);
+        for (int col = 0; col < inputLena.cols; ++col)
+        {
+            const uchar * uc_pixel = ptr;
+            int a = uc_pixel[0];
+            int b = uc_pixel[1];
+            int c = uc_pixel[2];
+            if(a != 0 && b != 0 && c != 0)
+                qDebug( "b: %d, g: %d, r:%d", a, b, c );
+            //sum += a + b + c;
+            qDebug( "b: %d, g: %d, r:%d", a, b, c );
+
+            ptr += 3;
+        }
+    }
+
+
 }
